b1d1b58cc35a3970db7ef56cea2f09352850ddde
Parag Verma <paragthegreat1@gmail.com>
2019-02-11 22:42:31 -0500

    Port 80

--- server/app.js
+++ server/app.js
@@ -28,6 +28,11 @@ app.get('*', function(req, res) {
 		res.sendFile(path.resolve('./client/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function (err) {
+  if(err){
+	  console.log("Could not start server at port 80");
+  }
+  else{
+	  console.log('Example app listening on port 80!');
+  }
 })
